eed7faffbf89c07c704b5b6995cdee063158a862
souzacef <125619939+souzacef@users.noreply.github.com>
2024-06-19 22:52:09 -0300

    19/06/2024 22:51

--- screematch/src/main/java/br/com/alura/screematch/ScreematchApplication.java
+++ screematch/src/main/java/br/com/alura/screematch/ScreematchApplication.java
@@ -1,11 +1,16 @@
 package br.com.alura.screematch;
 
+import br.com.alura.screematch.model.DadosEpisodio;
 import br.com.alura.screematch.model.DadosSerie;
+import br.com.alura.screematch.model.DadosTemporada;
 import br.com.alura.screematch.service.ConsumoAPI;
 import br.com.alura.screematch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreematchApplication implements CommandLineRunner {
@@ -24,5 +29,19 @@ public class ScreematchApplication implements CommandLineRunner {
 		ConverteDados conversor = new ConverteDados();
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+		json = consumoAPI.ObterDados("http://www.omdbapi.com/?t=gilmore+girls&Season=1&episode=2&apikey=9f11e664");
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i<=dados.totalTemporadas(); i++) {
+
+			json = consumoAPI.ObterDados("http://www.omdbapi.com/?t=gilmore+girls&Season=" + i + "&apikey=9f11e664");
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
+
 	}
 }
